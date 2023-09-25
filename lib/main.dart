bca1cdc7c07e1e37ea394f8d40b0d06ef36b6ca2
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2023-09-25 22:34:09 +0530

    create Material_Calculator stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(Material_Calculator());
+
+class Material_Calculator extends StatelessWidget {
+  const Material_Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
