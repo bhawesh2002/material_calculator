702b7217a07cbe1b8f4cf92f555aeebef5d09416
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2023-09-27 11:38:05 +0530

    move NumPad to numpad.dart

--- lib/components.dart
+++ lib/components.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
 
-class NumPad extends StatelessWidget {
-  const NumPad({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height * 0.7,
-      color: Colors.green.shade100,
-      child: GridView.count(
-        crossAxisCount: 3,
-      ),
-    );
-  }
-}
