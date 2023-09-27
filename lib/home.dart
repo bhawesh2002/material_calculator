c0c8d0761db64efa82afefe36d4d89166f67988d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2023-09-27 11:46:23 +0530

    remove const for non-const expressions

--- lib/home.dart
+++ lib/home.dart
@@ -16,7 +16,7 @@ class Home extends StatelessWidget {
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
             ),
-            const Positioned(bottom: 0, left: 0, child: NumPad())
+            Positioned(bottom: 0, left: 0, child: NumPad())
           ],
         ));
   }
