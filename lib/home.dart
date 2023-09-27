41da90ad7be7c980460da7254402d93deed16f36
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2023-09-27 11:27:42 +0530

    use Stack as body for Scaffold and add NumPad

--- lib/home.dart
+++ lib/home.dart
@@ -7,10 +7,17 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Material Calculator'),
-      ),
-      body: const Center(),
-    );
+        appBar: AppBar(
+          title: const Text('Material Calculator'),
+        ),
+        body: Stack(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+            const Positioned(bottom: 0, left: 0, child: NumPad())
+          ],
+        ));
   }
 }
