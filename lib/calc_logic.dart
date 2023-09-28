6628e881984ee58e665639f049390a9f4ce575da
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2023-09-28 08:11:41 +0530

    add nessary functions and update() GetXController

--- lib/calc_logic.dart
+++ lib/calc_logic.dart
@@ -1,3 +1,20 @@
 import 'package:get/get.dart';
 
-class CalcLogic extends GetxController {}
+class CalcLogic extends GetxController {
+  String exp = "";
+
+  void modifyEcp(String number) {
+    exp += number;
+    update();
+  }
+
+  void clearExp() {
+    exp = "";
+    update();
+  }
+
+  void delChar() {
+    exp = exp.substring(0, (exp.length - 1));
+    update();
+  }
+}
