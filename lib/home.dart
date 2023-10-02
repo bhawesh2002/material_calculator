0a6d46de56a35972be4a693fec773b7bb957b1bc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2023-10-02 21:50:38 +0530

    add numpad to the Home along with other widgets

--- lib/home.dart
+++ lib/home.dart
@@ -12,9 +12,40 @@ class Home extends StatelessWidget {
     return Scaffold(
         body: Stack(
       children: [
-        Container(
+        SizedBox(
           width: MediaQuery.of(context).size.width,
-        )
+          height: MediaQuery.of(context).size.height,
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          child: NumPad(),
+        ),
+        Positioned(
+          top: 100,
+          right: 20,
+          child: GetBuilder<CalcLogic>(
+            builder: (context) {
+              return Text(
+                calcLogic.exp,
+                textScaleFactor: 2,
+              );
+            },
+          ),
+        ),
+        Positioned(
+          top: 150,
+          right: 20,
+          child: GetBuilder<CalcLogic>(
+            builder: (context) {
+              return Text(
+                calcLogic.result,
+                textScaleFactor: 2.3,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              );
+            },
+          ),
+        ),
       ],
     ));
   }
