b1ea69d676697f75bbf9f1251579f31b24618587
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2023-10-02 09:45:38 +0530

    recreate Home

--- lib/home.dart
+++ lib/home.dart
@@ -9,44 +9,6 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-        ),
-        Positioned(
-          bottom: 0,
-          left: 0,
-          child: NumPad(),
-        ),
-        Positioned(
-          top: 100,
-          right: 20,
-          child: GetBuilder<CalcLogic>(
-            builder: (context) {
-              return Text(
-                calcLogic.exp,
-                textScaleFactor: 2,
-              );
-            },
-          ),
-        ),
-        Positioned(
-          top: 150,
-          right: 20,
-          child: GetBuilder<CalcLogic>(
-            builder: (context) {
-              return Text(
-                calcLogic.result,
-                textScaleFactor: 2.3,
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              );
-            },
-          ),
-        ),
-      ],
-    ));
+    return Scaffold();
   }
 }
